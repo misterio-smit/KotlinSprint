5f03d560aa9beb5147872f05551ec2f5355df148
Igor <misterrio@mail.ru>
2025-07-30 01:55:20 +0700

    KS-2-2 problem solving

---
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_2
+
+fun main() {
+    val permanentEmployees = 50
+    val permanentSalary = 30000
+    val interns = 30
+    val internSalary = 20000
+
+    val permanentExpenses = permanentEmployees * permanentSalary
+
+    val totalExpenses = permanentExpenses + (interns * internSalary)
+
+    val totalEmployees = permanentEmployees + interns
+
+    val averageSalary = totalExpenses / totalEmployees
+
+    println("Расходы на постоянных сотрудников: $permanentExpenses руб.")
+    println("Общие расходы после прихода стажеров: $totalExpenses руб.")
+    println("Средняя зарплата после устройства стажеров: $averageSalary руб.")
+}
