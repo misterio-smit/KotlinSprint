3cf0d8272152677be1d969bee3d524b4a1007e55
Igor <misterrio@mail.ru>
2025-07-30 01:09:58 +0700

    KS-2-1 the arithmetic mean

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,13 @@
+package lesson_2
+
+fun main() {
+    val student1 = 3f
+    val student2 = 4f
+    val student3 = 3f
+    val student4 = 5f
+
+    val amountOfPoints = student1 + student2 + student3 + student4
+    val averageScore: Float = amountOfPoints / 4
+
+    println(averageScore)
+}
