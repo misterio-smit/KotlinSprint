d63b11382ce3d4323614bfbf67cda1aadaae7f6b
Igor <misterrio@mail.ru>
2025-07-29 20:04:39 +0700

    KS-1-2 formatting the code

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -1,13 +1,13 @@
 package org.example.lesson_1
 
-fun main(){
-    val numberOfOrders :Int = 75
-    val thanksMessage:String = "Thank you for using our services"
+fun main() {
+    val numberOfOrders: Int = 75
+    val thanksMessage: String = "Thank you for using our services"
     println(numberOfOrders)
     println(thanksMessage)
 
-    var numberOfEmployees  = 2000
-     //   println("Колличество сотрудников :$numberOfEmployees")
-    numberOfEmployees  = 1999
+    var numberOfEmployees = 2000
+    //   println("Колличество сотрудников :$numberOfEmployees")
+    numberOfEmployees = 1999
     println("Колличество сотрудников :$numberOfEmployees")
 }
