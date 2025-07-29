e85e76c859d5d19db2d4ae973e6be007edb85f04
Igor <misterrio@mail.ru>
2025-07-29 23:59:03 +0700

    KS-1-3 changing the data type

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,14 +1,14 @@
 package org.example.lesson_1
 
 fun main() {
-    val yearOfFlight = 1961
+    val yearOfFlight = "1961"
     println("$yearOfFlight the year Yuri Gagarin made his first flight into space")
-    var hour = 9
-    var minute = 7
-    println("in 0$hour hour")
-    println("0$minute minute")
-    hour = 10
-    minute = 55
+    var hour = "09"
+    var minute = "07"
+    println("in $hour hour")
+    println("$minute minute")
+    hour = "10"
+    minute = "55"
     print("$hour:$minute")
 
 }
