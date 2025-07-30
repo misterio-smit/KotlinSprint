05fc55c0cd790066b9cbbd1fa8d45f3e89a9c730
Igor <misterrio@mail.ru>
2025-07-30 20:04:42 +0700

    KS-2-4 add constant

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,13 +1,16 @@
 package org.example.lesson_2
 
+//процент бафа
+const val BUFF_PERCENTAGE = 0.2
+
 fun main() {
     // количество добытых материалов без баффа
     val crystalsWithoutBuff = 7
     val ironWithBuff = 11
 
     // Рассчитываем бонусные материалы (20% от исходного количества)
-    val cristalBonus = (crystalsWithoutBuff * 0.2).toInt()
-    val ironBonus = (ironWithBuff * 0.2).toInt()
+    val cristalBonus = (crystalsWithoutBuff * BUFF_PERCENTAGE).toInt()
+    val ironBonus = (ironWithBuff * BUFF_PERCENTAGE).toInt()
 
     println("Бонусные кристаллические руды: $cristalBonus")
     println("Бонусные железные руды: $ironBonus")
