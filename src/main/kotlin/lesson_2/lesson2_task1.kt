bcf2a1542556361b34d6c0bbd5f0e967d945f8ad
Igor <misterrio@mail.ru>
2025-07-30 19:43:42 +0700

    KS-2-1 add amountStudent

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -5,9 +5,10 @@ fun main() {
     val student2 = 4f
     val student3 = 3f
     val student4 = 5f
+    val amountStudent = 4
 
     val amountOfPoints = student1 + student2 + student3 + student4
-    val averageScore: Float = amountOfPoints / 4
+    val averageScore: Float = amountOfPoints / amountStudent
 
     println(averageScore)
 }
