ef51d086b9866cd1efb8f6a34aa1356e18a02c21
Igor <misterrio@mail.ru>
2025-07-31 00:52:51 +0700

    KS-2-4 fixed buffPercentage

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,16 +1,15 @@
 package org.example.lesson_2
 
-//процент бафа
-const val BUFF_PERCENTAGE = 0.2
 
 fun main() {
     // количество добытых материалов без баффа
     val crystalsWithoutBuff = 7
     val ironWithBuff = 11
+    val buffPercentage = 20
 
     // Рассчитываем бонусные материалы (20% от исходного количества)
-    val cristalBonus = (crystalsWithoutBuff * BUFF_PERCENTAGE).toInt()
-    val ironBonus = (ironWithBuff * BUFF_PERCENTAGE).toInt()
+    val cristalBonus = (crystalsWithoutBuff * buffPercentage / 100)
+    val ironBonus = (ironWithBuff * buffPercentage / 100)
 
     println("Бонусные кристаллические руды: $cristalBonus")
     println("Бонусные железные руды: $ironBonus")
