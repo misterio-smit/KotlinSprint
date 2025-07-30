13cd03de8711dd6f3c8aac7bdae1f07b4a8d398f
Igor <misterrio@mail.ru>
2025-07-30 15:40:04 +0700

    KS-3-1 the greeting line

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val userName = "Кукуцаполь"
+    var greeting = ""
+
+    // Дневное приветствие
+    greeting = "Добрый день"
+    println("$greeting, $userName!")
+
+    // Вечернее приветствие
+    greeting = "Добрый вечер"
+    println("$greeting, $userName!")
+}
