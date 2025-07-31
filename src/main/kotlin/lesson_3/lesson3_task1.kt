1266be0e6fe823e2d235720b809ac301b6ebb450
Igor <misterrio@mail.ru>
2025-07-31 16:04:26 +0700

    KS-3-1 deleted unnecessary comments

--- src/main/kotlin/lesson_3/lesson3_task1.kt
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -3,13 +3,9 @@ package org.example.lesson_3
 fun main() {
 
     val userName = "Кукуцаполь"
-    var greeting = ""
-
-    // Дневное приветствие
-    greeting = "Добрый день"
+    var greeting = "Добрый день"
     println("$greeting, $userName!")
 
-    // Вечернее приветствие
     greeting = "Добрый вечер"
     println("$greeting, $userName!")
 }
