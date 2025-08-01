7cd44027eab6f38628b1ef752210eaef7481429a
Igor <misterrio@mail.ru>
2025-08-01 07:37:37 +0700

    KS-3-2 deleted unnecessary comments, corrected the comments

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -3,16 +3,14 @@ package org.example.lesson_3
 fun main() {
     val firstName = "Татьяна"
     val middleName = "Сергеевна"
-    val lastNameAt20 = "Андреева"
-    val lastNameAt22 = "Сидорова"
-    val ageAt20 = 20
-    val ageAt22 = 22
+    var lastName = "Андреева"
+    var age = 20
 
-    // ФИО до замужества
-    val fioAt20 = "$lastNameAt20 $firstName $middleName , $ageAt20"
-    println(fioAt20)
-
-    // ФИО после замужества
-    val fioAt22 = "$lastNameAt22 $firstName $middleName , $ageAt22"
-    println(fioAt22)
+    var fio = "$lastName $firstName $middleName , $age"
+    println(fio)
+    
+    lastName = "Сидорова"
+    age = 22
+    fio = "$lastName $firstName $middleName , $age"
+    println(fio)
 }
