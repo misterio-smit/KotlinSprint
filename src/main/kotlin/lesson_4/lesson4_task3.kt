ec2b600da5a880818577e80bcfe2a5c53a32965d
Igor <misterrio@mail.ru>
2025-08-03 22:51:24 +0700

    KS-4-3 Moved variables to constants

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -2,12 +2,12 @@ package org.example.lesson_4
 
 fun main() {
 
-    val isSunnyWeather = true
-    val isTentOpen = true
-    val humidity = 20
-    val currentSeason = "зима"
-
-    val favorableConditions = isSunnyWeather && isTentOpen && humidity == 20 && currentSeason != "зима"
+    val favorableConditions = IS_SUNNY_WEATHER && IS_TENT_OPEN && HUMIDITY == 20 && CURREN_SEASON != "не зима"
 
     println("Благоприятные ли условия сейчас для роста бобовых? $favorableConditions")
 }
+
+const val IS_SUNNY_WEATHER = true
+const val IS_TENT_OPEN = true
+const val HUMIDITY = 20
+const val CURREN_SEASON = "не зима"
