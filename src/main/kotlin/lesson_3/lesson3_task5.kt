cef3c0c283f1679f336fff0d63842a00c99e3800
Igor <misterrio@mail.ru>
2025-08-03 11:03:59 +0700

    KS-3-5 eliminated the remark

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -4,7 +4,6 @@ fun main() {
 
     val dataString = "D2-D4;0"
     val dataSplit = dataString.split("-", ";")
-    
     val from = dataSplit[0]
     val to = dataSplit[1]
     val moveNumber = dataSplit[2]
