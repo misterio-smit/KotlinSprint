b8497d118ef06237896e13f08fdf1ad405ca4ea9
Igor <misterrio@mail.ru>
2025-08-03 10:35:08 +0700

    KS-3-2 I deleted the comments and the extra variable.

--- src/main/kotlin/lesson_3/lesson3_task4.kt
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -2,19 +2,16 @@ package org.example.lesson_3
 
 fun main() {
 
-    // Первый ход: E2 -> E4
+
     var from = "E2"
     var to = "E4"
     var moveNumber = 1
 
-    val moveString = "[$from-$to;$moveNumber]"
-    println(moveString)
+    println("[$from-$to;$moveNumber]")
 
-    // Следующий ход белых: D2 -> D3
     from = "D2"
     to = "D3"
     moveNumber = 2
 
-    val nextMoveString = "[$from-$to;$moveNumber]"
-    println(nextMoveString)
+    println("[$from-$to;$moveNumber]")
 }
