b788e340f0f29f962599d2a00aae20ba81a81e3d
Igor <misterrio@mail.ru>
2025-08-07 22:41:43 +0700

    KS-5-6 when instead of if

--- src/main/kotlin/lesson_5/lesson5_task6.kt
+++ src/main/kotlin/lesson_5/lesson5_task6.kt
@@ -4,24 +4,26 @@ import kotlin.math.pow
 
 fun main() {
     print("Введите свой вес в килограммах:")
-    val userWeight = readLine()!!.toDouble()
+    val userWeight = readln().toDouble()
 
     print("Введите свой рост в сантиметрах:")
-    val userHeight = readLine()!!.toFloat() / 100
+    val userHeight = readln().toFloat() / 100
 
     val imt = userWeight / userHeight.pow(2)
 
+    when {
+        (imt < IMT_MIN) ->
+            println("Ваш ИМТ ${"%.2f".format(imt)}: Недостаточная масса тела")
 
-    if (imt < IMT_MIN)
-        println("Ваш ИМТ ${"%.2f".format(imt)}: Недостаточная масса тела")
-    else if (IMT_MIN <= imt && imt < IMT_NORMAL)
-        println("Ваш ИМТ ${"%.2f".format(imt)}: Нормальная масса тела")
-    else if (IMT_NORMAL <= imt && imt < IMT_MAX)
-        println("Ваш ИМТ ${"%.2f".format(imt)}: Избыточная масса тела")
-    else if (imt >= IMT_MAX)
-        println("Ваш ИМТ ${"%.2f".format(imt)}: Ожирение")
+        (IMT_MIN <= imt && imt < IMT_NORMAL) ->
+            println("Ваш ИМТ ${"%.2f".format(imt)}: Нормальная масса тела")
 
+        (IMT_NORMAL <= imt && imt < IMT_MAX) ->
+            println("Ваш ИМТ ${"%.2f".format(imt)}: Избыточная масса тела")
 
+        (imt >= IMT_MAX) ->
+            println("Ваш ИМТ ${"%.2f".format(imt)}: Ожирение")
+    }
 }
 
 const val IMT_MIN = 18.5
